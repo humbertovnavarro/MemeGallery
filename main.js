72499d65a6f35762b2cf89b0f46001b379089f77
humbertovnavarro <humberto.navarro.dev@gmail.com>
2021-06-19 12:23:55 -0700

    removed dummy data

--- main.js
+++ main.js
@@ -4,12 +4,13 @@ var gallery = document.querySelector('main');
 
 
 var imageUrls = [];
-
+/*
 imageUrls = [
   'https://i.kym-cdn.com/entries/icons/original/000/013/564/doge.jpg',
   'https://etcanada.com/wp-content/uploads/2016/06/58c0500b2ac4c342c6fc4ab096ada150.jpg?quality=80&strip=all&crop=0px%2C21px%2C500px%2C333px&resize=720%2C480',
   'https://wiki.godvillegame.com/images/thumb/7/76/Prap.jpg/298px-Prap.jpg'
 ];
+*/
 
 
 
